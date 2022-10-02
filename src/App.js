95247fa667f29d20b95328e94135b29c9fdfe197
joneseth1 <joneseth35@gmail.com>
2022-10-02 13:37:34 -0400

    More devlopement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useRef, Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { OrbitControls} from '@react-three/drei'
+import { FlyControls, OrbitControls, PerspectiveCamera} from '@react-three/drei'
 import Sphere from './components/AnimatedSphere';
 
 import './app.css'
+import { Camera } from 'three';
 
 
 export default function App() {
@@ -18,9 +19,13 @@ export default function App() {
         <div id='hero-description'>
           Computer Science Undergrad
         </div>
-        <div id='banner-wrapper'>
-            <Canvas>
-              <OrbitControls enableZoom={false}/>
+        <div class= 'banner-wrapper'>
+            <Canvas class = "morph-ball">
+              <OrbitControls enableZoom={false} enableDamping={true} target={[0,0,0]} enablePan={false} />
+              <PerspectiveCamera makeDefault
+              position={[500,1,2]}
+              fov={70}
+              zoom={100}/>
               <Suspense fallback={null}>
               <Sphere />
               </Suspense>
@@ -33,33 +38,6 @@ export default function App() {
 
 
      <div className='spacer wave'> </div>
-
-
-      
-      <div className='about-container'> 
-        <div id='about-header'>
-          About Me 
-        </div>
-
-        <div id='about-picture'> 
-        
-        </div>
-        
-        <div id='about-picture'>
-          Place holder
-        </div>
-
-        <div id='about-description'>
-          Seth started competitive cycling in 2014 when he raced at his local MTB series and started Road racing in 2015.
-          Seth struggled in the early parts of his cycling career but he would eventually find his footing and join his first professional team at 18. 
-          While Seth was out training in early 2020 he was hit by a car which would cause 8 compound fractures and muscle/tissue loss in his left leg, 2 fractured vertebrae, and a TBI caused by an intracranial hemorrhage. 
-          He spent a total of 22 days in the hospital, and started racing again in February of 2021. 
-          Since then he has raced across the world soaking up all the experience he could find. 
-          During his travels, Seth has been pursuing a BA in History and a BS in Computer Science with minors in Data Science and Bioinformatics at UNC Charlotte. 
-          Seth currently lives in Mooresville, North Carolina with his fiance, Hannah, and his cat, Honey.
-          He currently races for the Ferei-CCN-Metalac Pro team but is itching to get back into Mountain Biking and is looking to branch out to Gravel!
-        </div> 
-      </div>
 
 
     </div>
